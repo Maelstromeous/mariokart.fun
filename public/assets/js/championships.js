59dd695d3bda56ba7c76d2df92b8abc6993cdf60
Matt Cavanagh <maelstrome26@gmail.com>
2017-05-01 14:59:11 +0100

    Added code to populate vehicle image. Added title to new championship page

--- public/assets/js/championships.js
+++ public/assets/js/championships.js
@@ -1,41 +1,3 @@
-$('select[name^="character-"]').change(function () {
-  var size = $(this).find(':selected').attr('data-size');
-  var val = $(this).val();
-  var vehicleSelector = $(this).parents('.player-card').find('select[name^="vehicle-"]').first();
-  resetBars($(this));
-
-  // Reset options
-  vehicleSelector.find('option[data-size="' + size + '"]').prop('disabled', false).show();
-  vehicleSelector.val(0);
-  vehicleSelector.prop('disabled', true);
-
-  if (size) {
-    vehicleSelector.prop('disabled', false);
-    vehicleSelector.find('option[data-size!="' + size + '"]').prop('disabled', true).hide();
-  }
-
-  // Pull in image
-  var image = $(this).parents('.player-card').find('img.character').first();
-  if (val > 0) {
-    image.attr('src', assetUrl + '/img/characters/' + val + '.jpg' + version);
-  } else {
-    image.attr('src', assetUrl + '/img/GoldenMushie-Faded.png' + version);
-  }
-});
-
-$('select[name^="vehicle-"]').change(function () {
-  var vehicle = $(this).val();
-
-  // Reset any validation error colourings
-  $(this).css('background-color', '');
-
-  if (vehicle == '0') {
-    resetBars($(this));
-  }
-
-  setBars($(this), vehicle);
-});
-
 $('select[name^="player-"]').change(function () {
   // Reset any validation error colourings
   $(this).css('background-color', '');
@@ -71,59 +33,54 @@ $('select[name^="player-"]').change(function () {
   // Don't care if it fails
 });
 
-function resetBars(elem) {
-  var bars = $(elem).parents('.player-card').find('.progress-bar');
-  $(bars).each(function (index, el) {
-    $(this).css('width', '0px');
-    $(this).html('');
-  });
-}
+$('select[name^="character-"]').change(function () {
+  var size = $(this).find(':selected').attr('data-size');
+  var val = $(this).val();
+  var vehicleSelector = $(this).parents('.player-card').find('select[name^="vehicle-"]').first();
+  resetBars($(this));
 
-function setBars(elem, vehicle) {
-  var data = vehicleStats[vehicle];
-  var bars = $(elem).parents('.player-card').find('.progress-bar');
+  // Reset options
+  vehicleSelector.find('option[data-size="' + size + '"]').prop('disabled', false).show();
+  vehicleSelector.val(0);
+  vehicleSelector.prop('disabled', true);
+  vehicleSelector.trigger('change');
 
-  $(bars).each(function (index, el) {
-    var prop = $(this).attr('data-prop');
-    var min = $(this).attr('data-min') - 1; // - 2 so we can see the edge
-    var max = $(this).attr('data-max');
-    var stat = parseInt(vehicleStats[vehicle][prop]);
+  if (size) {
+    vehicleSelector.prop('disabled', false);
+    vehicleSelector.find('option[data-size!="' + size + '"]').prop('disabled', true).hide();
+  }
 
-    // Colour bars depending on good or bad
-    $(this).removeClass('bg-success bg-danger');
-    $(this).css('color', '#fff');
+  // Pull in image
+  var image = $(this).parents('.player-card').find('img.character').first();
+  if (val > 0) {
+    image.attr('src', assetUrl + '/img/characters/' + val + '.jpg' + version);
+  } else {
+    image.attr('src', assetUrl + '/img/GoldenMushie-Faded.png' + version);
+  }
+});
 
-    /* Calculate proper 100% width as each propery has a different maximum...
-       although on the wiki it says 80? ¯\_(ツ)_/¯ */
+$('select[name^="vehicle-"]').change(function () {
+  var val = parseInt($(this).val());
+  if (isNaN(val)) {
+    val = 0;
+  }
 
-    var statmod = stat - min;
-    var maxmod = max - min;
-    var width = (statmod / maxmod) * 100;
+  // Reset any validation error colourings
+  $(this).css('background-color', '');
 
-    // console.log(`(${statmod} / ${maxmod}) * 100 = ${width}`);
+  var image = $(this).parents('.player-card').find('img.vehicle').first();
+  if (val > 0) {
+    image.attr('src', assetUrl + '/img/vehicles/' + val + '.jpg' + version);
+  } else {
+    image.attr('src', assetUrl + '/img/GoldenMushie-Faded.png' + version);
+  }
 
-    $(this).css('width', width.toFixed(2) + '%');
-    $(this).html(stat);
-
-    if (prop === 'weight') {
-      if (width < 25) {
-        $(this).addClass('bg-success');
-      } else if (width > 75) {
-        $(this).addClass('bg-danger');
-      }
-    } else {
-      if (width > 75) {
-        $(this).addClass('bg-success');
-      } else if (width < 25) {
-        $(this).addClass('bg-danger');
-      }
-    }
-
-    if (width < 15) {
-      $(this).css('color', '#000');
-    }
-  });
-}
+  if (val == 0) {
+    resetBars($(this));
+  } else {
+    setBars($(this), val);
+  }
+});
 
 $('#submit').click(function () {
   $('#form-errors').html('').hide();
@@ -198,3 +155,58 @@ $('#submit').click(function () {
     $('#submit i').removeClass('fa-refresh fa-spin').addClass('fa-arrow-right');
   });
 });
+
+
+function resetBars(elem) {
+  var bars = $(elem).parents('.player-card').find('.progress-bar');
+  $(bars).each(function (index, el) {
+    $(this).css('width', '0px');
+    $(this).html('');
+  });
+}
+
+function setBars(elem, vehicle) {
+  var data = vehicleStats[vehicle];
+  var bars = $(elem).parents('.player-card').find('.progress-bar');
+
+  $(bars).each(function (index, el) {
+    var prop = $(this).attr('data-prop');
+    var min = $(this).attr('data-min') - 1; // - 2 so we can see the edge
+    var max = $(this).attr('data-max');
+    var stat = parseInt(vehicleStats[vehicle][prop]);
+
+    // Colour bars depending on good or bad
+    $(this).removeClass('bg-success bg-danger');
+    $(this).css('color', '#fff');
+
+    /* Calculate proper 100% width as each propery has a different maximum...
+       although on the wiki it says 80? ¯\_(ツ)_/¯ */
+
+    var statmod = stat - min;
+    var maxmod = max - min;
+    var width = (statmod / maxmod) * 100;
+
+    // console.log(`(${statmod} / ${maxmod}) * 100 = ${width}`);
+
+    $(this).css('width', width.toFixed(2) + '%');
+    $(this).html(stat);
+
+    if (prop === 'weight') {
+      if (width < 25) {
+        $(this).addClass('bg-success');
+      } else if (width > 75) {
+        $(this).addClass('bg-danger');
+      }
+    } else {
+      if (width > 75) {
+        $(this).addClass('bg-success');
+      } else if (width < 25) {
+        $(this).addClass('bg-danger');
+      }
+    }
+
+    if (width < 15) {
+      $(this).css('color', '#000');
+    }
+  });
+}
